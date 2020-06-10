de4f536af93c7b1e279419b2895fd75a9e92806d
Aghil Vinayak <aghilvinayak.p@gmail.com>
2020-06-10 09:34:08 +0530

    Run multiple threads

--- test/src/test.c
+++ test/src/test.c
@@ -30,55 +30,58 @@ int main(void) {
 #include "pthread.h"
 
 //  Sum computed by the background thread
-	long long sum = 0;
+struct sum_runner_struct {
+	long long limit;
+	long long answer;
+};
 
 //thread function to generate sum of 0 to N
 void* sum_runner( void* arg)
 {
-	long long *limit_ptr = (long long*) arg;
-	long long limit = *limit_ptr;
+	struct sum_runner_struct *arg_struct = (struct sum_runner_struct*) arg;	
+	long long sum = 0;
 
-
-	for(long long i = 0; i <= limit; i++)
+	for(long long i = 0; i <= arg_struct->limit; i++)
 	{
 		sum += i;
-		printf("\nIn runner thread limit %lld i %lld sum %lld", limit, i, sum);
+		//printf("\nIn runner thread limit %lld i %lld sum %lld", arg_struct->limit, i, sum);
 	}
 	//sum is global variable, so other threads can access
 	// do someting else
 	
+	arg_struct->answer = sum;
 	pthread_exit(0);
 }
 int main( int argc, char **argv)
 {
 	if(argc <2)
 	{
-		printf("\nUsage: %s <num>\n", argv[0]);
+		//printf("\nUsage: %s <num>\n", argv[0]);
 		return 0;
 	}
 
 	int num_args = argc - 1;
-	long long limit; 
+
+	struct sum_runner_struct args[num_args];
 
 	// Thread ID:
 	pthread_t tid[num_args];
 	for(int i = 0; i < num_args; i++)
 	{
-		limit = atoll(argv[i+1]);
-
+		args[i].limit = atoll(argv[i+1]);
 		// Create attribute
 		pthread_attr_t attr;
 		pthread_attr_init(&attr);
-		printf("\nIn main thread limit %lld i %d sum %lld", limit, i, sum);
-		pthread_create(&tid[i], &attr, sum_runner, &limit);
+		//printf("\nIn main thread limit %lld i %d  answer %lld", args[i].limit, i, args[i].answer);
+		pthread_create(&tid[i], &attr, sum_runner, &args[i]);
 	}
 
 	for(int i = 0; i < num_args; i++)
 	{
 		//Wait until thread is done its work
 		pthread_join(tid[i],NULL);
+		printf("\ni %d Sum is %lld\n", i, args[i].answer);
 	}
 
-	printf("\nSum is %lld\n", sum);
 
 }
