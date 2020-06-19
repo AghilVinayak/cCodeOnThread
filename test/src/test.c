d6f2b00ce1a00dfcf8ca23d43d1769ad135ba8ef
Aghil Vinayak <aghilvinayak.p@gmail.com>
2020-06-19 09:08:30 +0530

    mulit thread

--- test/src/test.c
+++ test/src/test.c
@@ -26,13 +26,11 @@ int main(void) {
 //============================================================================
 #include <stdio.h>
 #include <stdlib.h>
-//#include <windows.h>
-#include "pthread.h"
-
+#include <pthread.h>
 //  Sum computed by the background thread
 struct sum_runner_struct {
-	long long limit;
-	long long answer;
+	long long *limit;
+	long long *answer;
 };
 
 //thread function to generate sum of 0 to N
@@ -40,8 +38,14 @@ void* sum_runner( void* arg)
 {
 	struct sum_runner_struct *arg_struct = (struct sum_runner_struct*) arg;	
 	long long sum = 0;
+	long long limit;
+	long long *limitPtr = arg_struct->limit;
+	long long *answerPtr = arg_struct->answer;
+	limit = *limitPtr;
+	answerPtr = malloc(sizeof( *answerPtr));
+	free(limitPtr);
 
-	for(long long i = 0; i <= arg_struct->limit; i++)
+	for(long long i = 0; i <= limit; i++)
 	{
 		sum += i;
 		//printf("\nIn runner thread limit %lld i %lld sum %lld", arg_struct->limit, i, sum);
@@ -49,14 +53,14 @@ void* sum_runner( void* arg)
 	//sum is global variable, so other threads can access
 	// do someting else
 	
-	arg_struct->answer = sum;
-	pthread_exit(0);
+	*answerPtr = sum;
+	pthread_exit(answerPtr);
 }
 int main( int argc, char **argv)
 {
 	if(argc <2)
 	{
-		//printf("\nUsage: %s <num>\n", argv[0]);
+		printf("\nUsage: %s <num>\n", argv[0]);
 		return 0;
 	}
 
@@ -68,7 +72,8 @@ int main( int argc, char **argv)
 	pthread_t tid[num_args];
 	for(int i = 0; i < num_args; i++)
 	{
-		args[i].limit = atoll(argv[i+1]);
+		args[i].limit = malloc(sizeof(*args[i].limit ));
+		*args[i].limit = atoll(argv[i+1]);
 		// Create attribute
 		pthread_attr_t attr;
 		pthread_attr_init(&attr);
@@ -78,9 +83,11 @@ int main( int argc, char **argv)
 
 	for(int i = 0; i < num_args; i++)
 	{
+		long long *sum;
+		sum = malloc(sizeof(*sum));
 		//Wait until thread is done its work
-		pthread_join(tid[i],NULL);
-		printf("\ni %d Sum is %lld\n", i, args[i].answer);
+		pthread_join(tid[i], (void**)&sum);
+		printf("\ni %d tid %lu Sum is %lld\n", i, tid[i], *sum);
 	}
 
 
